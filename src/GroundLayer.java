bb439f46429b03a1d3a15b71c557e24468810611
bastienchevallier <bastienchevallier@192.168.0.10>
2017-10-12 12:35:27 +0200

    Correction of the code

--- src/GroundLayer.java
+++ src/GroundLayer.java
@@ -18,7 +18,7 @@ class Messenger implements Runnable{
 	
 	public void Messenger(){
 		this.socket = GroundLayer.socket;
-		this.AboveLayer = GroundLayer.AboveLayer;
+		this.AboveLayer = GroundLayer.getAboveLayer();
 	}
 	
 	public void run(){
@@ -42,7 +42,6 @@ class Messenger implements Runnable{
 
 public class GroundLayer {
 	public static DatagramSocket socket;
-	public static Layer AboveLayer;
 	/**
 	 * This {@code Charset} is used to convert between our Java native String
 	 * encoding and a chosen encoding for the effective payloads that fly over the
@@ -55,20 +54,12 @@ public class GroundLayer {
 	 * datagram. This allows to simulate the loss of packets in the network.
 	 */
 	public static double RELIABILITY = 1.0;
-
-
-  /**
-   * This value is used as the probability that {@code send} really sends a
-   * datagram. This allows to simulate the loss of packets in the network.
-   */
-  public static double RELIABILITY = 1.0;
-  private static Layer AboveLayer;
+	private static Layer AboveLayer;
 
 	public static boolean start(int localPort) {
 		try{
 			socket = new DatagramSocket(localPort);
 			Messenger _messenger = new Messenger();
-			
 			return true;
 		}catch(SocketException e){
 			System.out.println(e.getMessage());
@@ -78,17 +69,11 @@ public class GroundLayer {
 
 	}
 
-<<<<<<< HEAD
   public static void deliverTo(Layer layer) {
 		setAboveLayer(layer);
   }
-=======
-	public static void deliverTo(Layer layer) {
-		AboveLayer=
-	}
->>>>>>> branch 'master' of https://github.com/bastienchevallier/networks_lab4.1.git
 
-<<<<<<< HEAD
+
   public static void send(String payload, String destinationHost,
       int destinationPort) {
 	  try {
@@ -105,11 +90,6 @@ public class GroundLayer {
 		  System.err.println("Wrong destinationHost : "+e.getMessage());
 	  } 
   }
-=======
-	public static void send(String payload, String destinationHost,
-			int destinationPort) {
-	}
->>>>>>> branch 'master' of https://github.com/bastienchevallier/networks_lab4.1.git
 
 	public static void close() {
 		
